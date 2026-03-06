19418f9a247346bff01694ea139b8ea811420564
pacokwon <haechank@gmail.com>
2026-03-06 16:32:13 +0900

    test: Holmake first to build p4 related theories

--- hol/p4_from_json/run-tests.sh
+++ hol/p4_from_json/run-tests.sh
@@ -12,8 +12,11 @@ if [[ ! -d "$TESTDIR" ]]; then
     exit 1
 fi
 
+cp validation_tests/Holmakefile "$TESTDIR"
+cd "$TESTDIR"
+Holmake
+cd ..
 ./petr4_json_export.sh "$TESTDIR" p4include/
 ./petr4_to_hol4p4_dir.sh "$TESTDIR" 1
-cp validation_tests/Holmakefile "$TESTDIR"
 cd "$TESTDIR"
 Holmake -k
