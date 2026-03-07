30acc8e8ba0908ae4e784b85c3a585319cb5fcb4
pacokwon <haechank@gmail.com>
2026-03-07 10:29:14 +0900

    test: count passes at the end

--- hol/p4_from_json/run-tests.sh
+++ hol/p4_from_json/run-tests.sh
@@ -19,5 +19,14 @@ Holmake
 cd ..
 ./petr4_json_export.sh "$TESTDIR" p4include/
 ./petr4_to_hol4p4_dir.sh "$TESTDIR" 1
+mv petr4_to_hol4p4_stf.log "$TESTDIR"
 cd "$TESTDIR"
 Holmake -k
+
+PASS=$(ls "$TESTDIR"/.hol/objs/*.uo | wc -l)
+JSON_SUCCESS=$(find "$TESTDIR" -maxdepth 1 -name '*.json' -size +0c | wc -l)
+TOTAL=$(ls "$TESTDIR"/*.p4 | wc -l)
+
+echo "Total Tests: $TOTAL"
+echo "Successful JSON outputs: $JSON_SUCCESS/$TOTAL"
+echo "Pass: $PASS/$TOTAL"
